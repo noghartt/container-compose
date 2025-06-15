4b232631fc18326507a7eae980390162fdd87358
Guilherme Ananias <iamguilhermeananias@gmail.com>
2025-06-14 23:07:49 -0300

    chore: add Formula with brewfile to install CLI with brew

---
+++ Formula/container-compose.rb
@@ -0,0 +1,24 @@
+class ContainerCompose < Formula
+  desc "A docker-compose like tool for Apple Containers "
+  homepage "https://github.com/noghartt/container-compose"
+
+  version "0.0.1-alpha.2"
+
+  depends_on "socat"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/noghartt/container-compose/releases/download/v#{version}/container-compose-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "330a99ce22ef2e6d9db8e37d4735479a4135441bb9d053cfd71de2e6cdce8d3a"
+    end
+
+    on_intel do
+      url "https://github.com/noghartt/container-compose/releases/download/v#{version}/container-compose-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "e861f215d0ad23819fd27e485fe2c46f02e4446d7fc3b923c5cde05e6b8649d0"
+    end
+  end
+
+  def install
+    bin.install "container-compose"
+  end
+end
