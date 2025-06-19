0332baf88604a4fc1252a3b3d58a753e163f69b8
Guilherme <iamguilhermeananias@gmail.com>
2025-06-19 14:50:25 -0300

    build: update Formula to v0.0.1-alpha.5 (#25)

--- Formula/container-compose.rb
+++ Formula/container-compose.rb
@@ -2,19 +2,19 @@ class ContainerCompose < Formula
   desc "A docker-compose like tool for Apple Containers "
   homepage "https://github.com/noghartt/container-compose"
 
-  version "0.0.1-alpha.4"
+  version "0.0.1-alpha.5"
 
   depends_on "socat"
 
   on_macos do
     on_arm do
       url "https://github.com/noghartt/container-compose/releases/download/v#{version}/container-compose-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "4be414aaa3aa9d13a52e45b6f0d99be1cfd12e91b205cf7435e212947e7d7039"
+      sha256 "8b6f9d35b8a03eb14c059b347aa2b9e705e6c75c10496ba462a8cf7d58545973"
     end
 
     on_intel do
       url "https://github.com/noghartt/container-compose/releases/download/v#{version}/container-compose-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "320c5ca84e409629d053d39df84ec35916f3bafda10398fc215cd86d0fc97896"
+      sha256 "a409ca44b8cec7ac540c0db453f6f8e3d22f29c6cbb4f8cb21ecfe946011610f"
     end
   end
 
